140735b196e9fb5e01aec28988d836f02fabd322
Andras Belicza <iczaaa@gmail.com>
2015-01-24 03:34:37 +0100

    Added controls and view image to the client html. Also added client side javascript to refresh the view image periodically (defined by the FPS control). Basic handlers are now defined.

--- view/handlers.go
+++ view/handlers.go
@@ -1,20 +1,47 @@
 package view
 
 import (
+	"fmt"
 	"html/template"
+	"image"
+	"image/color"
+	"image/jpeg"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 var params = struct {
-	Title string
-}{"Labyrinth Demo"}
+	Title         string
+	Width, Height int
+	RunId         int64
+}{"Gopher's Labyrinth - First Gopher Gala (2015)", 600, 600, time.Now().Unix()}
 
 var playTempl = template.Must(template.New("t").Parse(play_html))
 
 func init() {
 	http.HandleFunc("/", playHtmlHandle)
+	http.HandleFunc("/runid", runIdHandle)
+	http.HandleFunc("/img", imgHandle)
 }
 
+// playHtmlHandle serves the html page where the user can play.
 func playHtmlHandle(w http.ResponseWriter, r *http.Request) {
 	playTempl.Execute(w, params)
+}
+
+// runidHandle serves the running app id which changes if app is restarted
+// (so browser clients can detect if app was restarted).
+func runIdHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%d", params.RunId)
+}
+
+// imgHandle serves images of the player's view.
+func imgHandle(w http.ResponseWriter, r *http.Request) {
+	quality, err := strconv.Atoi(r.FormValue("quality"))
+	if err != nil || quality < 0 || quality > 100 {
+		quality = 70
+	}
+
+	jpeg.Encode(w, image.NewUniform(color.RGBA{128, 128, 128, 255}), &jpeg.Options{quality})
 }
--- view/play_html.go
+++ view/play_html.go
@@ -6,6 +6,10 @@ const play_html = `<html>
 <style>
 	body           {padding: 0px; margin: 0px; text-align: center;}
 	#jsRequiredMsg {padding: 3px; margin: 3px; font-weight: bold; background: #faa;}
+	#controls      {padding: 2px;}
+	#view          {position: relative; padding: 1px;}
+	#img           {background: #000; border: 1px solid black;}
+	#errMsg        {visibility: hidden; position: absolute; top: 10px; right: 0px; width: 100%; color: #ff3030; font-weight: bold;}
 </style>
 </head>
 
@@ -17,9 +21,120 @@ const play_html = `<html>
 	</div>
 </noscript>
 
-Hi! Soon content comes here...
+<div id="controls">
+	Quality:
+	<select id="quality">
+		<option value="100">100</option>
+		<option value="90">90</option>
+		<option value="80">80</option>
+		<option value="70" selected>70</option>
+		<option value="60">60</option>
+		<option value="50">50</option>
+		<option value="40">40</option>
+		<option value="30">30</option>
+		<option value="20">20</option>
+		<option value="10">10</option>
+		<option value="0">0</option>
+	</select>
+	
+	FPS:
+	<select id="fps">
+		<option value="33">30</option>
+		<option value="40">25</option>
+		<option value="50" selected>20</option>
+		<option value="66">15</option>
+		<option value="100">10</option>
+		<option value="143">7</option>
+		<option value="200">5</option>
+		<option value="333">3</option>
+		<option value="500">2</option>
+		<option value="1000">1</option>
+	</select>
+	
+	<button id="pauseResume" onclick="pauseResume()">Pause</button>
+	
+	<a href="/cheat" target="_blank">Cheat</a>
+</div>
+
+<div id="view">
+	<img id="img" width="{{.Width}}" height="{{.Height}}"
+		onload="errMsg.style.visibility = 'hidden'; imgLoaded = true;"
+		onerror="errMsg.style.visibility = 'visible'; setTimeout('imgLoaded = true;', 1000);"
+		onmousedown="imgClicked(event)"/>
+	<div id="errMsg">Connection Error or Application Closed!</div>
+</div>
+
+<script>
+	var runId = {{.RunId}};
+	var playing = false, imgLoaded = true;
+	
+	// HTML elements:
+	var img            = document.getElementById("img"),
+		errMsg         = document.getElementById("errMsg"),
+		quality        = document.getElementById("quality"),
+		fps            = document.getElementById("fps"),
+		pauseResumeBtn = document.getElementById("pauseResume");
+	
+	// Disable image dragging and right-click context menu:
+	img.oncontextmenu = img.ondragstart = function() { return false; }
+	
+	pauseResume();
+	
+	// Kick-off:
+	refresh();
+	setInterval(checkRunId, 10000);
+	
+	function pauseResume() {
+		playing = !playing;
+		imgLoaded = true;
+		pauseResumeBtn.innerText = playing ? "Pause" : "Resume";
+	}
+	
+	function refresh() {
+		if (playing && imgLoaded) {
+			imgLoaded = false;
+			img.src = "/img?quality=" + quality.value + "&t=" + new Date().getTime();
+			setTimeout(refresh, fps.value);
+		}
+		else
+			setTimeout(refresh, 5);
+	}
+	
+	function imgClicked(e) {
+		if (!playing)
+			return;
+		// Relative mouse coordinates inside image:
+		var x, y;
+		if (document.all) { // For IE, this is enough (exact):
+			x = e.offsetX;
+			y = e.offsetY;
+		} else {            // For other browsers:
+	    	x = e.clientX;
+	    	y = e.clientY;
+	    	for (var el = img; el; el = el.offsetParent) {
+	    		x -= el.offsetLeft - el.scrollLeft + el.clientLeft;
+        		y -= el.offsetTop - el.scrollTop + el.clientTop;
+	    	}
+    	}
+    	
+		var r = new XMLHttpRequest();
+		r.open("GET", "/clicked?x=" + x + "&y=" + y + "&t=" + new Date().getTime(), true);
+		r.send(null);
+	}
+	
+	function checkRunId() {
+		if (!playing)
+			return;
+		var r = new XMLHttpRequest();
+		r.open("GET", "/runid?t=" + new Date().getTime(), true);
+		r.onreadystatechange = function() {
+			if (r.readyState == 4 && r.status == 200 && runId != r.responseText)
+				window.location.reload(); // App was restarted, reload page
+		};
+		r.send(null);
+	}
+</script>
 
 </body>
-
 </html>
 `
